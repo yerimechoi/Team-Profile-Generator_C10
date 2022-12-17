989919d4f313aee25ed636e18cea23c8bde4bfc6
yerimechoi <yerimechoi@gmail.com>
2022-12-16 22:52:59 -0500

    Added variables that connects to html, npm i & lib

---
+++ index.js
@@ -0,0 +1,8 @@
+const generateHTML = require('./dist/index.html');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const fs = require('fs');
+const inquirer = require('inquirer');
