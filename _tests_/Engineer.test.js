c96fc160aa08c7b18a7cfbc807adc20e083ac313
yerimechoi <yerimechoi@gmail.com>
2022-12-19 03:19:26 -0500

    wrote some tests for engineer.js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,9 +1,24 @@
-const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
-describe('Engineer', () => {
-    if(``, () => {
+const engineer = new Engineer('Esther', 253, 'esther@test.com', 'estherrr');
 
+describe('Engineer', () => {
+    it(`returns Engineer's name`, () => {
+        expect(engineer.name).toBe('Esther');
+  })
+});
+
+describe('Engineer.getEmail', () => {})
+
+
+describe('getGithub()', () => {
+    it(`returns github account`, () => {
+        expect(engineer.getGitHubAccount()).toBe('esther@test.com');
+    })
+});
+
+describe('Engineer', () => {
+    it(`returns correct role`, () => {
+        expect(engineer.getRole()).toBe('Engineer');
     });
-    
 });
